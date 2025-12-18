8d612578f4f5a8374e4727c72814c88693f7a7e2
Ryan Modisette <ryanmodisette@Ryans-MacBook-Pro.local>
2025-12-18 15:13:55 -0500

    Fixed a syntax error in the AppUser class

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -5,6 +5,6 @@ namespace API.Entities;
 public class AppUser
 {
     public string Id { get; set; } = Guid.NewGuid().ToString();
-    public required string DisplayName { get; set; };
-    public required string Email { get; set; };
+    public required string DisplayName { get; set; }
+    public required string Email { get; set; }
 }
