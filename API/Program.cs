ef8f9548d427ee7ba0a3de024b5fb35b9d80dddc
Ryan Modisette <modisetteryan@gmail.com>
2025-12-19 10:16:07 -0500

    Finished the Angular skeleton

--- API/Program.cs
+++ API/Program.cs
@@ -10,16 +10,20 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
-var app = builder.Build();
+var app = builder.Build(); 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment()) 
 {
 
 }
-
+app.UseCors(options =>
+{
+    options.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200");
+});
 
 app.MapControllers();
 
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,5 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,20 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('client');
-}
+export class App implements OnInit{
+  private http = inject(HttpClient); 
+  protected title = signal('Dating App');
+  protected members = signal<any>([]);
+  async ngOnInit() {
+      this.members.set(await this.getMembers());
+    }
+
+    async getMembers() {
+      try {
+        return lastValueFrom(this.http.get('https://localhost:5000/api/members'));
+      } catch (error) {
+        console.log(error);
+        return [];
+      } 
+    }
+  }
